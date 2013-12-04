a943328f261e6dfeb4b35e3cacb14a3b31e7fc8f
Dmitry Ivanchenko <dmitry.ivanchenko@zojax.com>
2013-12-04 18:57:32 +0200

    #561 add editor refresh

--- src/zojax/blogger/resources/dragdrop.js
+++ src/zojax/blogger/resources/dragdrop.js
@@ -5,28 +5,34 @@ $(document).ready(function() {
         $("#form-widgets-pages-buttons-remove").hide();
     }
     // Sort
-    $(".form-widgets-pages .multi-widget").children().each(function(index) {
-        if ($(this).attr('id') == 'form-widgets-pages-'+index+'-row') {
-            $('#form-widgets-pages-'+index+'-widgets-position').val(index);
-        }
-    });
+    var sortItems;
+    (sortItems =  function () {
+        $(".form-widgets-pages .multi-widget input.text-widget").each(function(index) {
+                $(this).val(index);
+            });
+    })();
 
-    $( ".form-widgets-pages .multi-widget" ).sortable({
-        connectWith: ".form-widgets-pages .multi-widget",
+    $( '.form-widgets-pages .multi-widget').sortable({
         axis: "y",
         update: function(event, ui) {
-            parent = ui.item.parent();
-            parent.children().each(function(index) {
-                var Id = $(this).attr('id');
-                Id = Id.replace('form-widgets-pages-', '');
-                Id = Id.replace('-row', '');
-                $('#form-widgets-pages-'+Id+'-widgets-position').val(index);
-            });
+            sortItems();
+            var NO_TOGGLE = false;
         },
-        start : function(){
+        start : function(event, ui){
             NO_TOGGLE = true;
+            if (typeof(tinyMCE) != 'undefined'){
+                tinyMCE.execCommand('mceRemoveControl', false, ui.item.find('textarea').attr('id'));
+            }
+        },
+        stop : function(event, ui){
+            if (typeof(tinyMCE) != 'undefined'){
+                tinyMCE.execCommand('mceAddControl', false, ui.item.find('textarea').attr('id'));
+                ui.item.find('a.mceResize').mousedown(function (){
+                    return false;
+                });
+            }
         }
-    }).disableSelection();
+    });
 
     // Expand-collapse
     $(".form-widgets-pages .multi-widget .row > div.widget").hide();
@@ -44,6 +50,9 @@ $(document).ready(function() {
 		    $glideElement.removeClass("closed");
 		    $glideElement.addClass("open");
 		    $glideElement.next().slideDown();
+            $("a.mceResize").mousedown(function (){
+                return false;
+            });
 	    } else {
 		    // hidde it
 		    $glideElement.removeClass("open");
@@ -63,4 +72,5 @@ $(document).ready(function() {
         }
     });
 
+    $('div.widget > script').remove()
 });
